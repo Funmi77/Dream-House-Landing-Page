eb0b8c69a160aa50b87a9682a6d17cf2e04d0147
Funmi77 <vrcreativeagency@gmail.com>
2026-05-22 21:25:25 +0100

    Improved API Response and Typewriter Speed

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 function showAnswer(response) {
   console.log(response.data.answer);
+
   let details = document.querySelector("#details");
-  details.innerHTML = "";
 
   new Typewriter("#details", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 20,
     cursor: "",
   });
 }
 
 let apiKey = "7e77fbbbab91e5504tfaaa75643of118";
+
 let prompt =
-  "Give me the best Architectural description of a Retro Modern Dream Home.";
-let context =
-  "Provide clear and precise answer in just four lines.Make it four lines.";
+  "Give me the best architectural description of a Retro Modern Dream Home.";
+
+let context = "Provide a clear and precise answer in exactly four lines.";
+
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 let button = document.querySelector("#getDetails");
-button.addEventListener("click", (event) => {
+
+button.addEventListener("click", function (event) {
   event.preventDefault();
+
+  let details = document.querySelector("#details");
+
+  details.innerHTML = "Generating dream home details...";
+
   axios.get(apiUrl).then(showAnswer);
 });
